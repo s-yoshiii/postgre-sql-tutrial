07260e5a26a3cb76cc745cfa8385171418dd5b8c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-08-24 23:57:06 +0900

    dbとの接続

---
+++ db.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool;
+const dotenv = require("dotenv");
+dotenv.config();
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "users",
+  password: process.env.PASS,
+  port: 5432,
+});
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const pool = require("./db");
 const app = express();
 const PORT = 5000;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) throw error;
+    return res.status(200).json(results.rows);
+  });
+});
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
